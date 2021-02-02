c9f0cc77fa4d89ff3d93e68fb182606674eae8fe
Timmy Douglas <timmy@timmyd.info>
2021-02-01 23:23:23 -0800

    syntax

--- timmy/coredns.scm
+++ timmy/coredns.scm
@@ -17,15 +17,15 @@
                     (commit commit)))
               (file-name (git-file-name name version))
               (sha256
-		base32 "02gdj866mz17p1f0fgfjpbb9cah2ykziacahpkw0viq1vj231hai"))))
+	       base32 "02gdj866mz17p1f0fgfjpbb9cah2ykziacahpkw0viq1vj231hai")))
      (build-system go-build-system)
      (arguments
       '(#:import-path "github.com/timmydo/coredns"))
      (synopsis "DNS server/forwarder, written in Go, that chains plugins")
-           (description "CoreDNS is a fast and flexible DNS server.
+     (description "CoreDNS is a fast and flexible DNS server.
   The key word here is flexible: with CoreDNS you are able to do
 what you want with your DNS data by utilizing plugins.  If some
 functionality is not provided out of the box you can add it by
 writing a plugin.")
-           (home-page "https://github.com/coredns/coredns")
-           (license license:asl2.0))))
+     (home-page "https://github.com/coredns/coredns")
+     (license license:asl2.0))))
