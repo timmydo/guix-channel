c1a4f4017d7dc38145349ac57c2db5269ba44873
Timmy Douglas <timmy@timmyd.info>
2023-01-27 16:39:21 -0800

    fix

--- channel/timmy/wireplumber.scm
+++ channel/timmy/wireplumber.scm
@@ -16,33 +16,8 @@
   #:use-module (gnu packages web)
   #:use-module (ice-9 match))
 
-(define-public wireplumber-timmy
+(define-public wireplumber-sans-elogind
   (package
-    (name "wireplumber-timmy")
-    (version "0.4.4")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://gitlab.freedesktop.org/pipewire/wireplumber")
-                    (commit version)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "0vr84xlnwlhrfq9l13rxh8cb8s389wlyafvi6bah444acmpx1lwv"))))
-    (build-system meson-build-system)
-    (arguments
-     `(#:configure-flags (list "-Dsystem-lua=true" "-Delogind=disabled")))
-    (native-inputs
-     `(("pkg-config" ,pkg-config)
-       ("glib-bin" ,glib "bin")))
-    (inputs
-     `(("dbus" ,dbus)
-       ("glib" ,glib)
-       ("lua" ,lua)
-       ("pipewire" ,pipewire-0.3)))
-    (home-page "https://pipewire.org/")
-    (synopsis "Modular session and policy manager for PipeWire")
-    (description "WirePlumber is a daemon that links streams from applications to their
-appropriate device.  For example WirePlumber could link an audio stream to a
-speaker.")
-    (license license:expat)))
+    (inherit wireplumber)
+    (inputs (modify-inputs (package-inputs wireplumber)
+                           (delete "elogind")))))
